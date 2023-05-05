6b90e35469aa635591653bfc8c1f4cd3f9715b04
unknown <jerez4959@gmail.com>
2023-05-05 12:52:48 -0400

    socaltotalaa

--- components/TablaGeneral.js
+++ components/TablaGeneral.js
@@ -12,11 +12,11 @@ const TablaGeneral = ({orden}) => {
     <thead>
       <tr class="bg-white text-gray-700 uppercase text-sm leading-normal">
         <th class=" px-4 py-2 hidden md:table-cell w-1/12">N° OC</th>
-        <th class=" px-4 py-2 hidden md:table-cell w-1/12">Fecha</th>
+        <th class=" px-4 py-2 hidden md:table-cell w-1/6">Fecha</th>
         <th class=" px-4 py-2 hidden md:table-cell w-1/6">Solicitante</th>
         <th class=" px-4 py-2 hidden md:table-cell w-1/6">Obra</th>
-        <th class=" px-4 py-2 hidden md:table-cell w-1/6">Proveedor</th>
         <th class=" px-4 py-2 hidden md:table-cell w-1/6">Total</th>
+        <th class=" px-4 py-2 hidden md:table-cell w-1/12">Detalle</th>
       </tr>
     </thead>
     
--- pages/listado-oc-general.js
+++ pages/listado-oc-general.js
@@ -5,6 +5,7 @@ import ListadoOcGeneral from '../components/ListadoOcGeneral'
 import TablaGeneral from '@/components/TablaGeneral'
 import * as XLSX from 'xlsx';
 import {useState, useEffect} from 'react'
+import {formatearDinero} from "helpers/index"
 
 
 
@@ -65,6 +66,87 @@ export default function Admin() {
     console.log(datos)
 
 
+
+
+    const [totalVolumen, setTotalVolumen] = useState(0);
+
+    
+    
+  
+
+    const totalgeneral = () => {
+      let suma = 0;
+      let totaldubiva = 0;
+    
+      results.forEach((ocpedidos) => {
+        const cantidad = ocpedidos.cantidad || 0;
+        const valor = ocpedidos.valor || 0;
+        const total = cantidad * valor;
+        const resultado = total === 0 ? 0 : total;
+    
+        const cantidad01 = ocpedidos.cantidad01 || 0;
+        const valor01 = ocpedidos.valor01 || 0;
+        const total01 = cantidad01 * valor01;
+        const resultado01 = total01 === 0 ? 0 : total01;
+    
+        const cantidad02 = ocpedidos.cantidad02 || 0;
+        const valor02 = ocpedidos.valor02 || 0;
+        const total02 = cantidad02 * valor02;
+        const resultado02 = total02 === 0 ? 0 : total02;
+    
+        const cantidad03 = ocpedidos.cantidad03 || 0;
+        const valor03 = ocpedidos.valor03 || 0;
+        const total03 = cantidad03 * valor03;
+        const resultado03 = total03 === 0 ? 0 : total03;
+    
+        const cantidad04 = ocpedidos.cantidad04 || 0;
+        const valor04 = ocpedidos.valor04 || 0;
+        const total04 = cantidad04 * valor04;
+        const resultado04 = total04 === 0 ? 0 : total04;
+    
+        const cantidad05 = ocpedidos.cantidad05 || 0;
+        const valor05 = ocpedidos.valor05 || 0;
+        const total05 = cantidad05 * valor05;
+        const resultado05 = total05 === 0 ? 0 : total05;
+    
+        const cantidad06 = ocpedidos.cantidad06 || 0;
+        const valor06 = ocpedidos.valor06 || 0;
+        const total06 = cantidad06 * valor06;
+        const resultado06 = total06 === 0 ? 0 : total06;
+    
+        const cantidad07 = ocpedidos.cantidad07 || 0;
+        const valor07 = ocpedidos.valor07 || 0;
+        const total07 = cantidad07 * valor07;
+        const resultado07 = total07 === 0 ? 0 : total07;
+    
+        const cantidad08 = ocpedidos.cantidad08 || 0;
+        const valor08 = ocpedidos.valor08 || 0;
+        const total08 = cantidad08 * valor08;
+        const resultado08 = total08 === 0 ? 0 : total08;
+    
+        const cantidad09 = ocpedidos.cantidad09 || 0;
+        const valor09 = ocpedidos.valor09 || 0;
+        const total09 = cantidad09 * valor09;
+        const resultado09 = total09 === 0 ? 0 : total09;
+    
+        suma += resultado + resultado01 + resultado02 + resultado03 + resultado04 + resultado05 + resultado06 + resultado07 + resultado08 + resultado09;
+      });
+    
+      const iva = suma * 0.19;
+      totaldubiva = suma + iva;
+      setTotalVolumen(totaldubiva);
+    };
+    
+
+
+    
+
+
+    function calcularVolumen() {
+        totalgeneral();
+      }
+
+
     
       
 
@@ -98,6 +180,13 @@ export default function Admin() {
 
             <div  className="text-center m-10">
                 <button onClick={() => exportTo(data)}>📥 Exportar a Excel</button>
+            </div>
+
+
+            <div className='flex justify-center items-center gap-2'>
+              <button className="my-4 py-2 px-4 text-black " onClick={calcularVolumen}>Calcular Volumen</button>
+              <p className="">{formatearDinero(totalVolumen)}</p>
+              
             </div>
 
         </LayoutInicioAdminGeneral>
